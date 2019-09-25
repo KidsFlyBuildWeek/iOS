b158caeae8d86f98d2c9648e6f034ad71f2f2d9f
Andrew Ruiz <nukedoom@gmail.com>
2019-09-25 14:46:37 -0700

    Added tableView

---
+++ KidsFly/KidsFly/View Controllers/HomeTripsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeTripsViewController.swift
+//  KidsFly
+//
+//  Created by Andrew Ruiz on 9/25/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class HomeTripsViewController: UIViewController {
+
+    @IBOutlet weak var myTripsTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func notifyButtonTapped(_ sender: Any) {
+    }
+}
