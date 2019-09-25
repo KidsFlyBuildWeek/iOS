f23f33f632ec37c82a9f228e294a8000852caa10
Andrew Ruiz <nukedoom@gmail.com>
2019-09-25 15:02:06 -0700

    Linked up buttons to tableViewCell file.

---
+++ KidsFly/KidsFly/Views/HomeTripsTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  HomeTripsTableViewCell.swift
+//  KidsFly
+//
+//  Created by Andrew Ruiz on 9/25/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class HomeTripsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var flightName: UILabel!
+    @IBOutlet weak var fromCityLabel: UILabel!
+    @IBOutlet weak var estimatedTimeOfArrival: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var destinationLabel: UILabel!
+    @IBOutlet weak var flightConnectionName: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+    @IBAction func seeInfoButtonTapped(_ sender: Any) {
+    }
+    
+}
