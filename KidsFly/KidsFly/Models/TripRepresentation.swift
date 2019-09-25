062d353da42bd33690a077fc1aa37bfa5fad7622
Andrew Ruiz <nukedoom@gmail.com>
2019-09-24 19:01:14 -0700

    Final commit

--- KidsFly/KidsFly/Models/TripRepresentation.swift
+++ KidsFly/KidsFly/Models/TripRepresentation.swift
@@ -10,8 +10,8 @@ import Foundation
 
 struct TripRepresentation: Codable {
     
-    var tripid: Int
+    var tripid: Int32
     var date: String
-    var passengercount: Int
+    var passengercount: Int32
     var luggagetype: String
 }
