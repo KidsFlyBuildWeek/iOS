c5b12aa97dfb8bfdd7d0f601a0d3b46775acaa63
Andrew Ruiz <nukedoom@gmail.com>
2019-09-25 14:03:36 -0700

    Added KidsFlyConnectionViewController

---
+++ KidsFly/KidsFly/View Controllers/KidsFlyConnectionApplyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  KidsFlyConnectionApplyViewController.swift
+//  KidsFly
+//
+//  Created by Andrew Ruiz on 9/25/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class KidsFlyConnectionApplyViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var tellUsAboutYourselfTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func felonySegmentedControl(_ sender: Any) {
+    }
+    
+    @IBAction func submitButtonPressed(_ sender: Any) {
+    }
+    
+}
